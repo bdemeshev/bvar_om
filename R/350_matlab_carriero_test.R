a8f78fdd6d83d99532d717ad933735e5307a706c
Boris Demeshev <boris.demeshev@gmail.com>
2015-08-07 20:21:40 +0300

    update bvar4

--- R/350_matlab_carriero_test.R
+++ R/350_matlab_carriero_test.R
@@ -1,6 +1,7 @@
 # read data from Carriero
 
 library("R.matlab")
+library("bvarr")
 all <- readMat("../data/usa_data.mat")
 str(all)
 
@@ -8,8 +9,10 @@ usa_data <- all$usa.data
 usa_data
 str(usa_data)
 
-priors <- Carriero_priors(Y_in = usa_data, p = 12, s2_lag = 1)
+priors <- Carriero_priors(Y_in = usa_data, p = 4, s2_lag = 1, lambdas = c(0.2,,))
 model <- bvar_conjugate0(priors=priors)
 forecasts <- forecast_conjugate(model, h=12)
 
+?Carriero_priors
 ?forecast_conjugate
+?bvar_conjugate0
