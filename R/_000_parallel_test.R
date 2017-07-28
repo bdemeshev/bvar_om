8435c7e0ef58619cab51fbbd23105d8862795507
Boris Demeshev <boris.demeshev@gmail.com>
2017-07-28 13:50:50 +0700

    parallel test

---
+++ R/_000_parallel_test.R
@@ -0,0 +1,20 @@
+my_fun <- function(x) {
+  a <- ggplot2::qplot(rnorm(100))
+  return(x^2)
+}
+
+library(doParallel)  
+
+export_functions <- c("my_fun")
+export_packages <- c("ggplot2", "forecast", "BigVAR")
+
+cluster <- makeCluster(4)
+registerDoParallel(cluster)
+
+foreach(i = 1:10000, 
+        .export = export_functions, 
+        .packages = export_packages) %dopar% {
+  r <- my_fun(i)
+}
+
+stopCluster(cluster)  
