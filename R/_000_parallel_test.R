b3754cdf4b875cdb5df4b8d635fa6b9431a61af2
Boris Demeshev <boris.demeshev@gmail.com>
2017-07-29 09:27:05 +0700

    more comments

--- R/_000_parallel_test.R
+++ R/_000_parallel_test.R
@@ -1,5 +1,7 @@
 # small parallel demo
 
+# it's a good idea to use plain R (not Rstudio) for parallel computations
+
 # this function will be used by all cores:
 my_fun <- function(x) {
   a <- ggplot2::qplot(rnorm(100))
@@ -13,6 +15,13 @@ export_functions <- c("my_fun")
 export_packages <- c("ggplot2", "forecast", "BigVAR")
 
 n_clusters <- 4
+# one may try to detect cores
+detectCores()
+# problems:
+# 1. this function is very platform dependent and may give wrong results 
+# better works for mac and linux
+# 2. optimal number of cores is usually lower than the total number of cores
+# so the optimal number of cores may be determined by experiments
 
 # "cat" and "message" function will be redirected to "log.txt"
 cluster <- makeCluster(n_clusters, outfile = "log.txt")
@@ -25,4 +34,4 @@ foreach(i = 1:10000,
   r <- my_fun(i)
 }
 
-stopCluster(cluster)  
+stopCluster(cluster)  # a must
