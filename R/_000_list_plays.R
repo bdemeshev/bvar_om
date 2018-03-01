51fdbfab6e9c271c777af5347ba9d5e34212146d
Boris Demeshev <boris.demeshev@gmail.com>
2018-03-01 12:14:56 +0300

    update 1 march

--- R/_000_list_plays.R
+++ R/_000_list_plays.R
@@ -9,6 +9,7 @@
 library(torro)
 library(tidyverse)
 library(forecast)
+# library(stringr)
 
 
 missing <- c(3773L, 3774L, 3775L, 3776L, 3777L, 3778L, 3779L, 3780L, 3781L, 
@@ -62,16 +63,106 @@ fits_long_2 <- forecast_all(fits_long_subset, var_sets, basefolder = basefolder,
                             n_cores = n_cores)
 
 
-# all forecasts to 1 file
+# save all forecasts to 1 file
 basefolder <- "~/Documents/bvar_om/estimation/refits/"
 all_forecasts <- get_forecasts_from_fit_files(basefolder = basefolder)
 write_rds(all_forecasts, paste0(basefolder, "all_forecasts.Rds"))
 
 # go on
+basefolder <- "~/Documents/bvar_om/estimation/refits/"
+
+fits_long <- create_fits_long(shifts, models, var_sets, horizons)
+
 all_forecasts <- read_rds(path = paste0(basefolder, "all_forecasts.Rds"))
 
-all_f2 <- attach_y_to_forecasts(all_forecasts, fits_long, rus_macro)
-glimpse(all_forecasts)
+all_f2 <- augment_forecasts(all_forecasts, fits_long, rus_macro)
+
+rmse_table <- calculate_rmse(all_f2, fits_long, 
+                             T_rmse_min = 133, 
+                             T_rmse_max = 243)
+# 133 # start 2006
+# 243 # last obs in rus_macro, in 2015
+
+
+
+# to check!!
+
+# 1. all models are estimated?
+# 2. intersecting arima sets? !ok!
+# 3. NA for last observations? !ok!
 
 
 glimpse(all_f2)
+
+
+test_arima <- all_f2 %>% filter(model_type == "arima", 
+                                pars_id == "automatic",
+                          row_number == 2, 
+                          variable == "ind_prod",
+                          var_set == "set_A")
+
+
+test_lasso <- all_f2 %>% filter(model_type == "var_lasso",
+                                pars_id == "OwnOther_3",
+                                row_number == 1, 
+                                variable == "ind_prod",
+                                var_set == "set_A")
+
+table(test_lasso$T_forecast)
+table(test_arima$T_forecast)
+
+setdiff(133:243, test_lasso$T_forecast)
+setdiff(133:243, test_arima$T_forecast)
+
+glimpse(fits_long)
+
+
+
+df <- rmse_table %>% filter(model_type == "arima", 
+                      var_set == "set_C", 
+                      variable == "ind_prod") 
+qplot(data = df, x = row_number, y = rmse)
+
+
+df <- rmse_table %>% filter(model_type == "ets", 
+                            var_set == "set_C", 
+                            variable == "ind_prod") 
+qplot(data = df, x = row_number, y = rmse)
+
+df <- rmse_table %>% filter(model_type == "rw", 
+                            var_set == "set_C", 
+                            variable == "ind_prod") 
+qplot(data = df, x = row_number, y = rmse)
+
+df <- rmse_table %>% filter(model_type == "var_lasso",
+                            pars_id == "OwnOther_1",
+                            var_set == "set_A", 
+                            variable == "ind_prod") 
+qplot(data = df, x = row_number, y = mae)
+
+
+
+unique(rmse_table$model_type)
+
+h_subset <- c(1, 3, 6, 9, 12)
+
+rmse_table$model_code <- str_c(rmse_table$model_type, ":", rmse_table$pars_id)
+
+
+rmse_table_wide <- reshape2::dcast(rmse_table, value.var = "rmse",
+                                   variable + row_number + var_set ~ model_code, 
+                                   fun.aggregate = mean)
+
+rmse_table_wide_1d <- filter(rmse_table_wide, 
+                             var_set == "set_C",
+                             row_number %in% h_subset) %>%
+          select(variable, row_number, `arima:automatic`, `rw:automatic`, `ets:automatic`)
+
+relmse_table_wide_1d <- mutate(rmse_table_wide_1d,
+                               arima = (`arima:automatic` / `rw:automatic`)^2,
+                               ets = (`ets:automatic` / `rw:automatic`)^2) %>% 
+                        select(variable, row_number, arima, ets)
+rel1d_melted <- reshape2::melt(relmse_table_wide_1d, id.vars = c("variable", "row_number"),
+                                variable.name = "model", value.name = "rel_mse")
+
+rel_comparison <- reshape2::dcast(rel1d_melted, variable ~ row_number + model)
